32d3001af54caf74fd67f3d3d0c0bd6be4404b61
User <dc2000dsl@aol.com>
2018-11-07 17:23:26 -0500

    initial checkin

--- routes/web_routes.js
+++ routes/web_routes.js
@@ -1,3 +1,7 @@
 module.exports = function(app, db) {
-
+ app.get('/*.enc', function (req, res) {
+    //this is only temporary since secret is for testing only
+    res.send('Unsupported');
+    console.log("Attempt to access server secret");
+  });
 };
